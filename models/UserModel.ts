457e26afafc93e54e94926297d42e279d53ec35c
Celia <celiamillanalemany@gmail.com>
2023-12-29 09:41:42 +0100

    creating user models

--- models/UserModel.ts
+++ models/UserModel.ts
@@ -28,19 +28,22 @@ const UserModel = {
         return users
 
     },
-    async getUser(userId){
+    async getUser(userId: number){
         // SELECT * FROM user WHERE id = userId;
+        const connection = await connectionDb();
+        const users = connection?.query(`SELECT * FROM user  WHERE id = ${userId}`)
+        return users
 
     },
-    async createUser(bodyData){
+    async createUser(bodyData : object){
         // INSERT INTO user (name, email, role, password) VALUES();
 
     },
-    async updateUser(userId, bodyData){
+    async updateUser(userId : number , bodyData : object){
         //UPDATE user SET name=?, email = ?, role = ? , password = ? ,WHERE id = userId;
 
     },
-    async deleteUser(userId){
+    async deleteUser(userId: number){
         //DELETE FROM user WHERE id = userID;
 
     },
