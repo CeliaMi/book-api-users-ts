09e005304a0a2a357c6f0c7ed814fb7fdfd990b8
Celia <celiamillanalemany@gmail.com>
2024-01-14 17:19:48 +0100

    complete user crud for admin

--- routes/UserRouter.ts
+++ routes/UserRouter.ts
@@ -2,6 +2,7 @@ import { Router } from "express"
 import userController from "../controllers/UserController"
 import { loginController, registerController } from "../controllers/AuthController";
 import { authMiddleware } from "../middleware/authMiddleware"; 
+import { checkRol } from "../middleware/rolMiddleware";
 
 
 const userRouter = Router();
@@ -9,9 +10,9 @@ const userRouter = Router();
 userRouter.post("/register", registerController);
 userRouter.post("/login", loginController);
 
-userRouter.get("/users", authMiddleware, userController.getUsers);
-userRouter.get("/users/:id", userController.getUser);
-userRouter.patch("/users/:id", userController.updateUser);
-userRouter.delete("/users/:id", userController.deleteUser);
+userRouter.get("/users", authMiddleware,checkRol(["admin"]), userController.getUsers);
+userRouter.get("/users/:id",authMiddleware, checkRol(["admin"]),userController.getUser);
+userRouter.patch("/users/:id",authMiddleware, checkRol(["admin"]), userController.updateUser);
+userRouter.delete("/users/:id",authMiddleware, checkRol(["admin"]), userController.deleteUser);
 
 export default userRouter;
